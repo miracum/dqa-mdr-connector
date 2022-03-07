2aef465c78d438fc930e75da851411cf4b9f80b9
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-03-07 12:09:11 +0100

    chore: added news.md

---
+++ NEWS.md
@@ -0,0 +1,23 @@
+# dqa-mdr-connector NEWS
+
+## Unreleased (2022-03-07)
+
+#### New Features
+
+* add split support for restricting_date_var / restricting_date_format
+* added data_map field
+* added plausi-relation to slot logic
+* download only set of dataelements
+#### Fixes
+
+* fixed write-scope for uploading
+* updated upload logic to consider only wanted dataelements
+* prepared arg for client_secret (wip)
+* working on migrating api to dehub-dev (wip)
+#### Others
+
+* added license statements to files
+* cleaning up python imports
+* moved confusing download-argument into UpdateMDR class
+* removed redundant code; ensure equivalence of key and variable_name
+* added comments to connector-files
--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 
 # dqa-mdr-connector: Connecting the MIRACUM-MDR with the DQA-Tool
 # Copyright (C) 2022 Universitätsklinikum Erlangen
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -24,6 +24,9 @@ __copyright__ = "Universitätsklinikum Erlangen"
 
 # create requirements from root directory:
 # pipreqs ./ --force
+
+# update news.md
+# auto-changelog -u -t "dqa-mdr-connector NEWS" --tag-prefix "v" -o "NEWS.md"
 
 from distutils.core import setup
 
